5c6f849f9f14f9b87c8d9dc44c9fc27ca30e9f9c
Filip Broniek <broniekf@gmail.com>
2022-01-14 18:38:06 +0100

    konspekt 12

--- activities/LoginActivity.kt
+++ activities/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.navigationdrawer.activities
 
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
@@ -9,9 +11,12 @@ import com.example.navigationdrawer.R
 
 class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val connection = isNetworkConnected()
+
+        disableLoginButton(connection)
     }
     fun saveLogin(view: View){
         val loginTextView = findViewById<TextView>(R.id.editTextLogin)
@@ -26,5 +31,19 @@ class LoginActivity : AppCompatActivity(){
         val editor = sharedPreferences.edit()
         editor.putString("MY_LOGIN_KEY", text)
         editor.apply()
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        return connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)!!.state == NetworkInfo.State.CONNECTED ||
+            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)!!.state == NetworkInfo.State.CONNECTED
+    }
+
+    private fun disableLoginButton(connection: Boolean) {
+        if (!connection) {
+            val button = findViewById<View>(R.id.buttonSetLogin)
+            button.isEnabled = false
+            findViewById<TextView>(R.id.textViewNoConnection).visibility = View.VISIBLE
+        }
     }
 }
